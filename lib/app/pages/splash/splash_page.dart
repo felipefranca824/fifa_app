1796c981e3cc7244b75c9b7f4f6e4fa9165f3b8f
Felipe França <felipe.franca@ws-solution.com>
2022-10-10 18:20:02 -0300

    create layout of splash page

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,6 +1,8 @@
 import 'package:fifa_book_app/app/core/ui/helpers/loader.dart';
 import 'package:fifa_book_app/app/core/ui/helpers/messages.dart';
 import 'package:fifa_book_app/app/core/ui/styles/button_styles.dart';
+import 'package:fifa_book_app/app/core/ui/styles/colors_app.dart';
+import 'package:fifa_book_app/app/core/ui/styles/text_styles.dart';
 import 'package:fifa_book_app/app/core/ui/widgets/button.dart';
 import 'package:flutter/material.dart';
 
@@ -15,40 +17,52 @@ class _SplashPageState extends State<SplashPage>
     with Loader<SplashPage>, Messages<SplashPage> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash Page'),
-      ),
-      body: Column(
-        children: [
-          Button.primary(
-            label: 'Salvar',
-            onPressed: () async {
-              showLoader();
-              await Future.delayed(Duration(seconds: 3));
-              hideLoader();
-            },
+      backgroundColor: context.colors.primary,
+      body: DecoratedBox(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'assets/images/background_splash.png',
+            ),
+            fit: BoxFit.cover,
           ),
-          Button.primary(
-            label: 'teste 1',
-            onPressed: () async {
-              showError("Erro no botão");
-            },
-          ),
-
-           Button.primary(
-            label: 'teste 2',
-            onPressed: () async {
-              showSuccess("Erro no botão 2 ");
-            },
-          ),
-           Button.primary(
-            label: 'teste 3',
-            onPressed: () async {
-              showInfo("Erro no botão 3");
-            },
-          ),
-        ],
+        ),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: EdgeInsets.only(top: size.height * .08),
+                child: Image.asset(
+                  'assets/images/fifa_logo.png',
+                  height: size.height * .25,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.only(bottom: size.height * .19),
+                child: Button(
+                  label: "Acessar",
+                  style: context.buttonStyles.yellowButton,
+                  width: size.width * .9,
+                  onPressed: () {},
+                  labelStyle:
+                      context.textStyles.textSecondaryFontExtraBoldPrimaryColor,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: Image.asset('assets/images/bandeiras.png')),
+            )
+          ],
+        ),
       ),
     );
   }
