e14a03097ce2a5ca5008b8aa937c51e659f8a8f2
Felipe França <felipe.franca@ws-solution.com>
2022-10-13 21:46:13 -0300

    implemented splash page

--- lib/app/pages/splash/presenter/splash_presenter_impl.dart
+++ lib/app/pages/splash/presenter/splash_presenter_impl.dart
@@ -1,13 +1,16 @@
 import 'package:fifa_book_app/app/pages/splash/presenter/splash_presenter.dart';
 import 'package:fifa_book_app/app/pages/splash/view/splash_view.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashPresenterImpl implements SplashPresenter {
   late final SplashView _view;
   @override
   Future<void> checkLogin() async {
     _view.showLoader();
-    await Future.delayed(Duration(seconds: 2));
-    _view.logged(false);
+    final accessToken = await SharedPreferences.getInstance()
+        .then((value) => value.getString('accessToken'));
+
+    _view.logged(accessToken != null);
   }
 
   @override
